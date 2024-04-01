497c07947d8ac46768337adc098f6c3c5351f43a
kejo <kj.insigne@gmail.com>
2024-04-01 14:44:30 +0800

    Setup Program.cs

--- FlexiSourceIT/Program.cs
+++ FlexiSourceIT/Program.cs
@@ -1,7 +1,12 @@
+using FlexiSourceIT.Controllers;
+using FlexiSourceIT.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddTransient<RainfallController>();
+builder.Services.AddTransient<IRainfallService, RainfallService>();
+builder.Services.AddHttpClient();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
