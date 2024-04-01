b858da718da84b23b36a08a1e338144ebc3d5dad
kejo <kj.insigne@gmail.com>
2024-04-01 14:12:51 +0800

    Add error response models

---
+++ FlexiSourceIT/Models/ErrorDetail.cs
@@ -0,0 +1,8 @@
+﻿namespace FlexiSourceIT.Models
+{
+    public class ErrorDetail
+    {
+        public string? PropertyName { get; set; }
+        public string? Message { get; set; }
+    }
+}
---
+++ FlexiSourceIT/Models/ErrorResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace FlexiSourceIT.Models
+{
+    public class ErrorResponse
+    {
+        public string? Message { get; set; }
+        public List<ErrorDetail>? Details { get; set; }
+    }
+}
